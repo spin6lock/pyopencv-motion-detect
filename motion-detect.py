60ee1b441c94244f9ccea12cfea6cd62fe4d3787
John Luk <john.37@gmail.com>
2012-01-01 19:37:37 +0800

    taking photos every second for ten times

--- motion-detect.py
+++ motion-detect.py
@@ -1,8 +1,9 @@
 # -*- coding:utf8 -*-
 
 from opencv import highgui as hg
-capture = hg.cvCreateCameraCapture(0)
+capture= hg.cvCreateCameraCapture(0)
 hg.cvNamedWindow("Snapshot")
-frame = hg.cvQueryFrame(capture)
-hg.cvShowImage("Snapshot", frame)
-hg.cvWaitKey(10000)
+for i in range(10):
+	frame = hg.cvQueryFrame(capture)
+	hg.cvShowImage("Snapshot", frame)
+	hg.cvWaitKey(1000)
