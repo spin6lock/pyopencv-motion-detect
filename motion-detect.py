b8fd058bd5baeb37b47c570d4fbd726e47557dee
John Luk <john.37@gmail.com>
2012-01-02 10:09:39 +0800

    It must be something wrong, the capture only capture one photo, so the frame sub always 0

--- motion-detect.py
+++ motion-detect.py
@@ -1,9 +1,25 @@
 # -*- coding:utf8 -*-
-
+import opencv
 from opencv import highgui as hg
-capture= hg.cvCreateCameraCapture(0)
+capture = hg.cvCreateCameraCapture(0)
 hg.cvNamedWindow("Snapshot")
+frames = []
 for i in range(10):
 	frame = hg.cvQueryFrame(capture)
+	frames.append(frame)
 	hg.cvShowImage("Snapshot", frame)
+hg.cvWaitKey(1000)
+
+hg.cvNamedWindow("hello")
+for i in range(10):
+	hg.cvShowImage("hello", frames[i])
 	hg.cvWaitKey(1000)
+"""
+import copy
+dst=copy.copy(frames[1])
+opencv.cvSub(frames[2], frames[1], dst)
+hg.cvShowImage("Snapshot", dst)
+from IPython.Shell import IPShellEmbed
+IPShellEmbed()()
+hg.cvWaitKey(10000)
+"""
