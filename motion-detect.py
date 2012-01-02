3422445cb8a13c510e9dafea9a5e4a793b60ea87
John Luk <john.37@gmail.com>
2012-01-02 10:30:21 +0800

    caution: the copy of python wrapper is not workgit add motion-detect.py Keep in mind that operation must be done on the low level c/c++ object

--- motion-detect.py
+++ motion-detect.py
@@ -1,9 +1,20 @@
 # -*- coding:utf8 -*-
 
+import opencv
 from opencv import highgui as hg
+
 capture= hg.cvCreateCameraCapture(0)
 hg.cvNamedWindow("Snapshot")
+
+frames = []
 for i in range(10):
 	frame = hg.cvQueryFrame(capture)
+	frames.append(opencv.cvClone(frame))
 	hg.cvShowImage("Snapshot", frame)
+	hg.cvWaitKey(1000)
+
+hg.cvNamedWindow("hello")
+for i in range(10):
+	frame = frames[i]
+	hg.cvShowImage("hello", frame)
 	hg.cvWaitKey(1000)
